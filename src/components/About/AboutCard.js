69664151224679cfa84ab55df6faec379b22c15f
Rajesh J <dev.rajesh142@gmail.com>
2025-03-09 15:00:04 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,25 +8,20 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hello Everyone, I am <span className="purple"> Rajesh </span>
-            from <span className="purple"> Hyderabad, India.</span>
-            <br />
-            I am currently working as a Freelancer.
-            <br />
-             My stream of Education is Copmuter Science Engineering (AI & ML)
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
+            Hi there! I'm Rajesh, a freelancer based in Hyderabad, India.
+            With a background in Computer Science Engineering focused on AI & ML, I specialize in crafting innovative solutions through technology. Currently, I'm working as a freelancer, helping businesses and individuals build impactful projects.
+            
+            When I'm not immersed in coding, I love pushing my limits and exploring new experiences. Some of my passions include:
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> On A Learning Spree
+              <ImPointRight /> Exploring New Technologies and diving into the latest trends in AI & ML
             </li>
             <li className="about-activity">
-              <ImPointRight /> Combact Sports (Wushu)
+              <ImPointRight /> Combat Sports (Wushu) for staying active and sharpening my focus
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight /> Travelling to experience new cultures and broaden my horizons
             </li>
           </ul>
         </blockquote>
